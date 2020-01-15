5c01ad7bbcb0c581e38e2345de9b2f1127ea8211
Tony Scimone <ajscimone@live.com>
2020-01-15 11:58:50 -0700

    Clean ApplicationController variable scope, and describe use of ApplicationController as a Sinatra::Base.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Create
 curl -i -H "Content-Type: application/json" -X POST -d '{"id":4, "title":"Walk", "description": "Take a walk"}' http://127.0.0.1:9393/task/new
 
 Update
-curl -i -H "Content-Type: application/json" -X PUT -d '{"title":"Finish this program", "description": "" "done":true}' http://localhost:9393/task/2
+curl -i -H "Content-Type: application/json" -X PUT -d '{"title":"Finish this program", "description": "", "done":true}' http://localhost:9393/task/2
 
 update
 
@@ -13,4 +13,10 @@ Delete
 curl -i -H "Content-Type: application/json" -X DELETE http://localhost:9393/api/delete/2
 
 http://127.0.0.1:9393/task/1
+
+
+
+https://medium.com/@salmaeng71/sinatra-todo-application-with-mvc-85f2997f2d17
+
+http://www.notearsonlydreams.io/blog/2015/05/13/babys-first-sinatra-app/
 -->
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,28 @@
 require 'date'
 require './config/environment'
 
+class Task_manager
+
+  @@count = 0
+  def self.count
+    @@count
+  end
+
+  def initialize()
+    task1 = {}
+    @task2 = 2
+    @@task3 = 3 # Every instance of Task_manager will share this 
+  end # task1 scope ends here
+
+  #task2 available here
+end 
+
 class ApplicationController < Sinatra::Base
 
-  @@task1 = { :id => 1, :title =>'Run Enery Plus', :description => 'Finish tweaking model for RTF or something like that', :done => false, :date => DateTime.now()  }
-  @@task2 = { :id => 2, :title =>'Run Enery Plus Again', :description => 'Finish tweaking model for TRC?', :done => false, :date => DateTime.now()  }
-  @@task3 = { :id => 3, :title =>'Run Enery Plus a third time', :description => 'I\'m out of ideas', :done => false, :date => DateTime.now()  }
-  @@tasks = [@@task1, @@task2, @@task3] 
+  task1 = { :id => 1, :title =>'Run Enery Plus', :description => 'Finish tweaking model for RTF or something like that', :done => false, :date => DateTime.now()  }
+  task2 = { :id => 2, :title =>'Run Enery Plus Again', :description => 'Finish tweaking model for TRC?', :done => false, :date => DateTime.now()  }
+  task3 = { :id => 3, :title =>'Run Enery Plus a third time', :description => 'I\'m out of ideas', :done => false, :date => DateTime.now()  }
+  @@tasks = [task1, task2, task3] 
 
   get "/" do
     'Hello world'
@@ -17,16 +33,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/task/:id" do
-    @id = params[:id]
-    @task = @@tasks.find {|task| task[:id] == @id.to_i }
-    @task.to_json
+    id = params[:id]
+    task = @tasks.find {|task| task[:id] == id.to_i }
+    task.to_json
   end
 
   post "/task/new" do
-    @new_task = { :id => params[:id].to_i, :title =>params[:title], :Description => params[:description], :done => false, :date => DateTime.now()  }
+    new_task = { :id => params[:id].to_i, :title =>params[:title], :Description => params[:description], :done => false, :date => DateTime.now()  }
     #@@tasks.push(@new_task)
-    @@tasks << @new_task
-    puts @@tasks
+    @tasks << @new_task
+    puts @tasks
     "Success\n"
   end
 
@@ -36,9 +52,24 @@ class ApplicationController < Sinatra::Base
     #@task.update(@task) { |key, value| value = params[:key]}
     #@task.update (:description => params[:description])
     #S@task.update (:done => params[:done])
-    @task = { :id => params[:id].to_i, :title => params[:title], :description => params[:description], :done => params[:done], :date => DateTime.now()  }
-    @@tasks[params[:id].to_i] = @task
-    @task.to_json
+    task = { :id => params[:id].to_i, :title => params[:title], :description => params[:description], :done => params[:done], :date => DateTime.now()  }
+    @tasks[params[:id].to_i] = task
+    task.to_json
   end
 
+  # delete '/task/:id' do
+  #   @id = params[:id]
+
+  #   @task = { :id => params[:id].to_i, :title => params[:title], :description => params[:description], :done => params[:done], :date => DateTime.now()  }
+  #   @@tasks[params[:id].to_i] = @task
+  #   @task.to_json
+  # end
 end
+
+# In order to run this program as a script, uncomment the following line:
+# run ApplicationController.run!
+
+#   Sinatra note:
+#     The current scaffolding of this project is set up to not run an instance of Application Controller class, but instead to use the class definition object
+#     defined above. Thus, the @@ is needed for any persistant variables since any instance variables will not get made (because an instance is never created)
+#
