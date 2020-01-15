ad3066d485a0fc588387f5e453b1e363d098d521
Tony Scimone <ajscimone@live.com>
2020-01-15 13:02:08 -0700

    Separate data logic into a Task_Manager class, implement get all, get by id, and new methods. These methods are hooked up to appropriate routes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,69 +1,37 @@
-require 'date'
+require './app/controllers/task_manager'
 require './config/environment'
 
-class Task_manager
-
-  @@count = 0
-  def self.count
-    @@count
-  end
-
-  def initialize()
-    task1 = {}
-    @task2 = 2
-    @@task3 = 3 # Every instance of Task_manager will share this 
-  end # task1 scope ends here
-
-  #task2 available here
-end 
-
 class ApplicationController < Sinatra::Base
-
-  task1 = { :id => 1, :title =>'Run Enery Plus', :description => 'Finish tweaking model for RTF or something like that', :done => false, :date => DateTime.now()  }
-  task2 = { :id => 2, :title =>'Run Enery Plus Again', :description => 'Finish tweaking model for TRC?', :done => false, :date => DateTime.now()  }
-  task3 = { :id => 3, :title =>'Run Enery Plus a third time', :description => 'I\'m out of ideas', :done => false, :date => DateTime.now()  }
-  @@tasks = [task1, task2, task3] 
+  
+  @@tasks = Task_Manager.new()
 
   get "/" do
     'Hello world'
   end
 
   get "/task/all" do
-    @@tasks.to_json
+    tasks = @@tasks.get_all_tasks()
   end
 
   get "/task/:id" do
     id = params[:id]
-    task = @tasks.find {|task| task[:id] == id.to_i }
-    task.to_json
+    task = @@tasks.get_task(id.to_i)
   end
 
   post "/task/new" do
-    new_task = { :id => params[:id].to_i, :title =>params[:title], :Description => params[:description], :done => false, :date => DateTime.now()  }
-    #@@tasks.push(@new_task)
-    @tasks << @new_task
-    puts @tasks
-    "Success\n"
+    task_params = JSON.parse(request.body.read)
+    response = @@tasks.add_task(task_params["title"], task_params["description"])
   end
 
   put '/task/:id' do
-    @id = params[:id]
-    #@task = @@tasks.find {|task| task[:id] == @id.to_i }
-    #@task.update(@task) { |key, value| value = params[:key]}
-    #@task.update (:description => params[:description])
-    #S@task.update (:done => params[:done])
-    task = { :id => params[:id].to_i, :title => params[:title], :description => params[:description], :done => params[:done], :date => DateTime.now()  }
-    @tasks[params[:id].to_i] = task
-    task.to_json
+    id = params[:id]
+    @@tasks.update_task(id.to_i)
   end
 
-  # delete '/task/:id' do
-  #   @id = params[:id]
-
-  #   @task = { :id => params[:id].to_i, :title => params[:title], :description => params[:description], :done => params[:done], :date => DateTime.now()  }
-  #   @@tasks[params[:id].to_i] = @task
-  #   @task.to_json
-  # end
+  delete '/task/:id' do
+    id = params[:id]
+    @@tasks.delete_task(id.to_i)
+  end
 end
 
 # In order to run this program as a script, uncomment the following line:
---
+++ app/controllers/task_manager.rb
@@ -0,0 +1,54 @@
+require 'date'
+
+class Task_Manager
+
+    def initialize()
+        task1 = { :id => 1, :title =>'Run Enery Plus', :description => 'Finish tweaking model for RTF or something like that', :done => false, :date => DateTime.now()  }
+        task2 = { :id => 2, :title =>'Run Enery Plus Again', :description => 'Finish tweaking model for TRC?', :done => false, :date => DateTime.now()  }
+        task3 = { :id => 3, :title =>'Run Enery Plus a third time', :description => 'I\'m out of ideas', :done => false, :date => DateTime.now()  }
+        @tasks = [task1, task2, task3]
+        @id_count = 3
+    end
+  
+    def get_all_tasks()
+        @tasks.to_json
+    end
+
+    def get_task(id)
+        if id.is_a? Integer
+            task = @tasks.select {|task| task[:id] == id }
+            return task.to_json
+        else
+            return "Not Found".to_json
+        end
+    end
+
+    def add_task(title, description="No description")
+        task = {}
+        if !title.is_a? String
+            return "Error: A title string is required for a task"
+        else
+            task[:title] = title
+        end
+        @id_count +=1
+        task[:id] = @id_count
+        task[:description] = description
+        task[:done] = false 
+        task[:date] = DateTime.now()
+        puts task
+        @tasks.push(task)
+        return @tasks.to_json
+    end
+
+    def update_task(id, title=nil, description=nil, done=nil)
+        return "not ready yet"
+    end
+
+    def delete_task(id)
+        return "not ready yet"
+    end
+  end 
+
+
+
+
